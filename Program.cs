e9cf0b35e1a70993ea69c40252bbf0b808307173
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-25 19:58:54 +0530

    Angram checking for two strings

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ namespace Algorithm_Problems
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to algorithm problems");
-            Merge_Sort mergeSort = new Merge_Sort();
-            mergeSort.UserInput();
+            Anagram anagram = new Anagram("heart", "earth");
         }
     }
 }
