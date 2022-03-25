18c779c499268aced1a81cf8fd850b8119f5fe79
Gangadevi Ch <chilamakooru.gangaiiit@gmail.com>
2022-03-25 19:20:10 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Algorithm_Problems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to algorithm problems");
+        }
+    }
+}
